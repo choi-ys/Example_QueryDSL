714477ea015503180c39e3c8c3ce0b2494f6262a
choy-ys <project.log.062@gmail.com>
2021-04-13 12:47:44 +0900

    [2021-04-13]용석:Package 구조 변경

--- src/main/java/io/example/querydsl/repository/custom/MemberSpringDataJpaRepositoryImpl.java
+++ src/main/java/io/example/querydsl/repository/custom/MemberSpringDataJpaRepositoryImpl.java
@@ -1,4 +1,4 @@
-package io.example.querydsl.repository;
+package io.example.querydsl.repository.custom;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
